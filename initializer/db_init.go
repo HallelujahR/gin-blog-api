fe08b02343ed13280160c870575952179b3a93d7
wangruiwen <13001130055@163.com>
2026-04-23 10:46:28 +0800

    fix: skip category migration on existing schemas

--- initializer/db_init.go
+++ initializer/db_init.go
@@ -7,14 +7,9 @@ import (
 
 func InitDB() {
 	db := database.GetDB()
-	if err := db.Exec("SET FOREIGN_KEY_CHECKS = 0").Error; err != nil {
-		panic("关闭外键检查失败: " + err.Error())
-	}
-
-	if err := db.AutoMigrate(
+	migrations := []interface{}{
 		&models.User{},
 		&models.UserSession{},
-		&models.Category{},
 		&models.Tag{},
 		&models.Post{},
 		&models.PostCategory{},
@@ -28,12 +23,15 @@ func InitDB() {
 		&models.TrafficSnapshot{},
 		&models.ImageCompressStats{},
 		&models.ImageCompressJob{},
-	); err != nil {
-		_ = db.Exec("SET FOREIGN_KEY_CHECKS = 1").Error
-		panic("数据库自动迁移失败: " + err.Error())
 	}
 
-	if err := db.Exec("SET FOREIGN_KEY_CHECKS = 1").Error; err != nil {
-		panic("恢复外键检查失败: " + err.Error())
+	if !db.Migrator().HasTable(&models.Category{}) {
+		migrations = append([]interface{}{&models.Category{}}, migrations...)
+	}
+
+	for _, model := range migrations {
+		if err := db.AutoMigrate(model); err != nil {
+			panic("数据库自动迁移失败: " + err.Error())
+		}
 	}
 }
