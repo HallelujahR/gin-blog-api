9727e69aaf969057fb23d9e1681fc951fd2a22ca
wangruiwen <13001130055@163.com>
2026-04-23 10:25:10 +0800

    fix: disable foreign key checks during migration

--- initializer/db_init.go
+++ initializer/db_init.go
@@ -7,6 +7,10 @@ import (
 
 func InitDB() {
 	db := database.GetDB()
+	if err := db.Exec("SET FOREIGN_KEY_CHECKS = 0").Error; err != nil {
+		panic("关闭外键检查失败: " + err.Error())
+	}
+
 	if err := db.AutoMigrate(
 		&models.User{},
 		&models.UserSession{},
@@ -25,6 +29,11 @@ func InitDB() {
 		&models.ImageCompressStats{},
 		&models.ImageCompressJob{},
 	); err != nil {
+		_ = db.Exec("SET FOREIGN_KEY_CHECKS = 1").Error
 		panic("数据库自动迁移失败: " + err.Error())
+	}
+
+	if err := db.Exec("SET FOREIGN_KEY_CHECKS = 1").Error; err != nil {
+		panic("恢复外键检查失败: " + err.Error())
 	}
 }
